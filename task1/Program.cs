96970ff45984f16a4c95c2b30126b636a45d3d73
Pavel Bobylev <paha_ska_15@mail.ru>
2022-05-05 10:07:05 +0300

    Добавьте файлы проекта.

---
+++ task1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число для расчёта факториала: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            GetFactorialAsync(n);
+            Console.ReadKey();
+        }
+        static void GetFactorial (int n)
+        {
+            int result = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                result *= i;
+                Thread.Sleep(10);
+            }
+            Console.WriteLine($"Факториал числа {n} равен: {result}");
+        }
+
+        static async void GetFactorialAsync(int n)
+        {
+            await Task.Run(()=> GetFactorial(n));
+        }
+    }
+}
